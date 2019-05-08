f55ecc90ea342aa06aa04b10eb60fd52bfb308d7
Halimo <halimoghelle@gmail.com>
2019-05-08 08:31:27 -0400

    destructured state/props

--- client/src/components/Loading.js
+++ client/src/components/Loading.js
@@ -1,41 +1,38 @@
-import React, {Component} from 'react';
-import '../loading.css'
-import {Spinner} from 'react-bootstrap';
-import backgroundLoading from '../collabloading.jpg';
-import { Link, withRouter } from 'react-router-dom';
+import React, { Component } from "react";
+import "../loading.css";
+import { Spinner } from "react-bootstrap";
+import backgroundLoading from "../collabloading.jpg";
+import { withRouter } from "react-router-dom";
+
 class Loading extends Component {
-  constructor(props) {
-    super(props);
+  render() {
+    const { labels } = this.props;
+    labels.pop();
+    const keyword = labels.map(word => {
+      return <h4 className="keywordDetected">{word}</h4>;
+    });
+    return (
+      <div>
+        <img className="loadingBackground" src={backgroundLoading} />
+
+        <div className="keywordSection">
+          <h2 className="keywordTitle">Keywords </h2>
+          <div className="dots">
+            <Spinner animation="grow" variant="success" />
+            <Spinner animation="grow" variant="success" />
+            <Spinner animation="grow" variant="success" />
+          </div>
+          {keyword}
+          <br />
+          <br />
+          <br />
+
+          <br />
+          <br />
+          <br />
+        </div>
+      </div>
+    );
   }
-  render () {
-    console.log(this.props)
-    this.props.labels.pop()
- const keyword = this.props.labels.map((word)=>{
-   return <h4 className="keywordDetected">{word}</h4>
- })
-  return (
-    <div >
-     <img className="loadingBackground" src={backgroundLoading} />
-
-    <div className="keywordSection">
-    <h2 className="keywordTitle">Keywords </h2>
-    <div className="dots">
-      <Spinner animation="grow" variant="success" />
-      <Spinner animation="grow" variant="success" />
-      <Spinner animation="grow" variant="success" />
-  </div>
-    {keyword}
-    <br/>
-    <br/>
-    <br/>
-
-      <br/>
-      <br/>
-      <br/>
-     </div>
-
-    </div>
-  );
-};
 }
-export default withRouter (Loading);
+export default withRouter(Loading);
--- client/src/components/Messages.js
+++ client/src/components/Messages.js
@@ -1,106 +1,108 @@
-import React, { Component } from 'react';
-import '../messages.css'
-import {Form, Row, Col, Button, Container} from 'react-bootstrap';
-import Navigation from './Navbar.js'
+import React, { Component } from "react";
+import "../messages.css";
+import { Form, Row, Col, Container } from "react-bootstrap";
+import Navigation from "./Navbar.js";
 
-function Chat (props){
- if(props.user.type === "influencer"){
-  return ( 
-    <div>
-    <div className="influencer-section">
-    <h4 className="message-photographer">Start chatting with Joe!</h4>
-  </div>
-  <div className="photographer-section">
-
-  </div>
-  </div>)
-
- } else {
-  return ( 
-    <div>
-    <div className="influencer-section">
-    <h4 className="message-influencer">Start chatting with Eloise</h4>
-  </div>
-  <div className="photographer-section">
-  </div>
-  </div>)
-
- }
-  
+function Chat(props) {
+  const { type } = props.user;
+  if (type === "influencer") {
+    return (
+      <div>
+        <div className="influencer-section">
+          <h4 className="message-photographer">Start chatting with Joe!</h4>
+        </div>
+        <div className="photographer-section" />
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <div className="influencer-section">
+          <h4 className="message-influencer">Start chatting with Eloise</h4>
+        </div>
+        <div className="photographer-section" />
+      </div>
+    );
+  }
 }
 
-class Messages extends Component {
-  constructor () {
-    super ();
+export default class Messages extends Component {
+  constructor() {
+    super();
     this.state = {
-      messages:[]
-    }
-   
+      messages: null
     };
-  componentDidMount () {
-    if (this.props.user.type === "influencer"){
-        var id = this.props.user.id
-        var url = '/'+id+'/influencerrequest'
-        fetch(url)
+  }
+  componentDidMount() {
+    const { type, id } = this.props.user;
+    if (type === "influencer") {
+      const url = "/" + id + "/influencerrequest";
+      fetch(url)
         .then(results => {
-           return results.json();
+          return results.json();
         })
-        .then(data => { this.setState({messages:data})
+        .then(data => {
+          this.setState({ messages: data });
         })
-        .catch(function (error) {
+        .catch(function(error) {
           console.log(error);
-        })}
-        else{
-          var id = this.props.user.id
-          var url = '/'+id+'/photographerrequest'
-          fetch(url)
-          .then(results => {
-             return results.json();
-          })
-          .then(data => { this.setState({messages:data})
-          })
-          .catch(function (error) {
-            console.log(error);
-          })}   
+        });
+    } else {
+      const url = "/" + id + "/photographerrequest";
+      fetch(url)
+        .then(results => {
+          return results.json();
+        })
+        .then(data => {
+          this.setState({ messages: data });
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
     }
-    
-    
-  render () {
-  let message = this.state.messages.map(function(message){
-    return (<div className="message-request">
-    <img className="message-photo" src={message.profilepic} />
-    <h4 className="message-name">{message.name}</h4></div>)
-  })
+  }
 
-  
-  return (
-  <div>
-  <Navigation user={this.props.user}/>
-  <Container>
-  <Row>
-    <Col sm={4} className="messages-left">
-    <h4 className="your-connections">Your <span className="connections">Connections</span></h4>
-      
-        {message}
-       
-    </Col>
-    <Col sm={8} className="messages-right">
-     <Chat user={this.props.user} />
+  render() {
+    const { messages } = this.state;
+    const { user } = this.props;
+    let message = messages.map(function(message) {
+      return (
+        <div className="message-request">
+          <img className="message-photo" src={message.profilepic} />
+          <h4 className="message-name">{message.name}</h4>
+        </div>
+      );
+    });
 
-    <div className="textarea">
-    <Form>
-      <Form.Group controlId="exampleForm.ControlInput1">
-        <Form.Control type="text" placeholder="Type your message here" />
-      </Form.Group>
-    </Form>
-     </div>
-    </Col>
-  </Row>
-</Container>
-</div>
-    )
-  };
+    return (
+      <div>
+        <Navigation user={user} />
+        <Container>
+          <Row>
+            <Col sm={4} className="messages-left">
+              <h4 className="your-connections">
+                Your <span className="connections">Connections</span>
+              </h4>
 
+              {message}
+            </Col>
+            <Col sm={8} className="messages-right">
+              <Chat user={user} />
+
+              <div className="textarea">
+                <Form>
+                  <Form.Group controlId="exampleForm.ControlInput1">
+                    <Form.Control
+                      type="text"
+                      placeholder="Type your message here"
+                    />
+                  </Form.Group>
+                </Form>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
-
-export default Messages;
