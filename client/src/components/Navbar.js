58a643a265c17a477132677a3fc4f9471693ccc0
Halimo <halimoghelle@gmail.com>
2019-05-08 08:29:02 -0400

    moved conditionals within component return and specified markup within conditional

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,54 +1,45 @@
-import React, { Component } from 'react';
-import '../navbar.css'
-import {Navbar} from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import "../navbar.css";
+import { Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
+export default class Navigation extends Component {
+  render() {
+    const type = this.props.user;
+    return (
+      <div>
+        <Navbar className="nav-bar">
+          <Navbar.Brand className="brand">Collab</Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>
+              <span className="icons">
+                <Link to="/user">
+                  <i class="fas fa-user" title="PROFILE" />
+                </Link>
+              </span>
+              {type === "influencer" && (
+                <span className="icons">
+                  <Link to="/upload">
+                    <i class="fab fa-searchengin" title="UPLOAD" />
+                  </Link>
+                </span>
+              )}
 
-class Navigation extends Component {
-  constructor () {
-    super ();
-    }
-render () {
-if(this.props.user.type === "influencer"){
-
-  return (
-    <div>
-    <Navbar className="nav-bar">
-      <Navbar.Brand className="brand">Collab</Navbar.Brand>
-      <Navbar.Toggle />
-      <Navbar.Collapse className="justify-content-end">
-      <Navbar.Text>
-       <span className="icons"><Link to="/photographer"><i class="fas fa-user" title="PROFILE"></i></Link></span>
-       <span className="icons"><Link to="/upload"><i class="fab fa-searchengin" title="UPLOAD"></i></Link></span>
-       <span className="icons"><Link to="/pending"><i class="fas fa-users" title="CONNECTION"></i></Link></span>
-       <span className="icons"><Link to="/messages"><i class="fas fa-envelope" title="MESSAGE"></i></Link></span>
-      </Navbar.Text>
-      </Navbar.Collapse>
-    </Navbar>
-    </div>
-    )
-
-} else {
-
-  return (
-    <div>
-    <Navbar className="nav-bar">
-      <Navbar.Brand className="brand">Collab</Navbar.Brand>
-      <Navbar.Toggle />
-      <Navbar.Collapse className="justify-content-end">
-      <Navbar.Text>
-        <span className="icons"><Link to="/photographer"><i class="fas fa-user" title="PROFILE"></i></Link></span>
-        <span className="icons"><Link to="/requests"><i class="fas fa-users" title="CONNECTION"></i></Link></span>
-        <span className="icons"><Link to="/messages"><i class="fas fa-envelope" title="MESSAGE"></i></Link></span>
-      </Navbar.Text>
-      </Navbar.Collapse>
-    </Navbar>
-    </div>
-    )
-
+              <span className="icons">
+                <Link to="/pending">
+                  <i class="fas fa-users" title="CONNECTION" />
+                </Link>
+              </span>
+              <span className="icons">
+                <Link to="/messages">
+                  <i class="fas fa-envelope" title="MESSAGE" />
+                </Link>
+              </span>
+            </Navbar.Text>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+    );
+  }
 }
-
-  };
-}
-
-export default Navigation;
