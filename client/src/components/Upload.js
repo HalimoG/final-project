3e1bc82ea9134dcde4e5ce6d77d4b0b53ee74b8c
Halimo <halimoghelle@gmail.com>
2019-05-08 08:22:46 -0400

    fixed syntax and destructured props

--- client/src/components/Upload.js
+++ client/src/components/Upload.js
@@ -1,79 +1,80 @@
-import React from 'react';
-import '../upload.css';
-import backgroundUpload from '../collabupload-copy.jpg';
-import {Form, Row, Col, Container, Button} from 'react-bootstrap';
-import Navigation from './Navbar.js'
-import { Redirect } from 'react-router-dom';
-const axios = require("axios");
-const elasticsearch = require('elasticsearch')
+import React from "react";
+import "../upload.css";
+import backgroundUpload from "../collabupload-copy.jpg";
+import { Row, Col, Container } from "react-bootstrap";
+import Navigation from "./Navbar.js";
+import { Redirect } from "react-router-dom";
+import axios from "axios";
 
-
-
-class Upload extends React.Component {
+export default class Upload extends React.Component {
   constructor(props) {
     super(props);
-    this.state ={
-        file: null
-  
+    this.state = {
+      file: null
     };
+  }
 
-}
-
-onFormSubmit = (e) =>{
-
+  onFormSubmit = e => {
     e.preventDefault();
     const formData = new FormData();
-    formData.append('myImage',this.state.file);
+    formData.append("myImage", this.state.file);
     const config = {
-        headers: {
-            'content-type': 'multipart/form-data'
-        }
+      headers: {
+        "content-type": "multipart/form-data"
+      }
     };
-    axios.post("/upload",formData,config)
-        .then((response) => {
-          console.log(response.data)
-         return this.props.setRedirectUpload(response.data.img, response.data.match)
-  })
+    axios.post("/upload", formData, config).then(response => {
+      console.log(response.data);
+      return this.props.setRedirectUpload(
+        response.data.img,
+        response.data.match
+      );
+    });
+  };
 
-}
-
-  onChange = (e) => {
-    this.setState({file:e.target.files[0]});
-}
+  onChange = e => {
+    this.setState({ file: e.target.files[0] });
+  };
 
   render() {
-   
-    if (!this.props.fireRedirect) {
+    const { fireRedirect } = this.props;
+    if (!fireRedirect) {
+      return (
+        <Redirect
+          to={{
+            pathname: "/results",
+            state: {
+              match: this.props.match,
+              keywords: this.props.labels
+            }
+          }}
+        />
+      );
+    }
 
-      return (<Redirect to={{
-            pathname:"/results",
-            state: { 
-              match:this.props.match,
-              keywords:this.props.labels}
-        }} />
-       )}
-
-  return (
-  <div className="upload-page">
-<Navigation user={this.props.user}/>
-<Container>
-  <Row>
-    <Col sm={4}>
-      <p className="upload"> Which <span className="styles">styles</span> are you feeling today? </p>
-      <form onSubmit={this.onFormSubmit}>
-          <h1 className="file-upload">File Upload</h1>
-          <input type="file" name="myImage" onChange= {this.onChange} />
-          <button type="submit">Upload</button>
-      </form>
-    </Col>
-    <Col sm={8}>
-      <img className="background-upload" src={backgroundUpload} />
-    </Col>
-    </Row>
-  </Container>
-  </div>
-  );
+    return (
+      <div className="upload-page">
+        <Navigation user={this.props.user} />
+        <Container>
+          <Row>
+            <Col sm={4}>
+              <p className="upload">
+                {" "}
+                Which <span className="styles">styles</span> are you feeling
+                today?{" "}
+              </p>
+              <form onSubmit={this.onFormSubmit}>
+                <h1 className="file-upload">File Upload</h1>
+                <input type="file" name="myImage" onChange={this.onChange} />
+                <button type="submit">Upload</button>
+              </form>
+            </Col>
+            <Col sm={8}>
+              <img className="background-upload" src={backgroundUpload} />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
   }
 }
-
-export default Upload;
