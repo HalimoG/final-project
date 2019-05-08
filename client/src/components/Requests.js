7ea3d41917221f296e711689c41b27cdf7f53770
Halimo <halimoghelle@gmail.com>
2019-05-08 08:26:26 -0400

    destructured state/props

--- client/src/components/Requests.js
+++ client/src/components/Requests.js
@@ -1,90 +1,94 @@
-import React, {Component} from 'react';
-import '../requests.css';
-import requestBackground from '../collabrequests.jpg';
-import {Row, Col, Container, Button, Card} from 'react-bootstrap';
-import Navigation from './Navbar.js'
-import { Link } from 'react-router-dom';
-const axios = require("axios");
+import React, { Component } from "react";
+import "../requests.css";
+import requestBackground from "../collabrequests.jpg";
+import { Row, Col, Container, Button } from "react-bootstrap";
+import Navigation from "./Navbar.js";
 
-class Requests extends Component {
+export default class Requests extends Component {
   constructor(props) {
     super(props);
-    this.state={
-      requestmade:[]
-    }
+    this.state = {
+      requestmade: null
+    };
   }
-  componentDidMount () {
-    var id = this.props.user.id
-    var url = '/'+id+'/photographerrequest'
+  componentDidMount() {
+    var id = this.props.user.id;
+    var url = "/" + id + "/photographerrequest";
     fetch(url)
-    .then(results => {
-      return results.json();
-    })
-    .then(data => { this.setState({requestmade:data})
-    })
-    .catch(function (error) {
-      console.log(error);
+      .then(results => {
+        return results.json();
+      })
+      .then(data => {
+        this.setState({ requestmade: data });
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
+
+  render() {
+    const { requestmade } = this.state;
+    const { connected, deleted } = this.props;
+    let request = requestmade.map((request, i) => {
+      if (connected) {
+        return (
+          <div className="request-box">
+            <img className="request-photo" src={request.profilepic} />
+            <h4 className="request-name">{request.name}</h4>
+            <span className="request-buttons">
+              <span>Connected!</span>
+            </span>
+          </div>
+        );
+      } else if (deleted) {
+        return (
+          <div>
+            <h1>No Pending Messages</h1>
+          </div>
+        );
+      } else {
+        return (
+          <div className="request-box">
+            <img className="request-photo" src={request.profilepic} />
+            <h4 className="request-name">{request.name}</h4>
+            <span className="request-buttons">
+              <Button
+                onClick={this.props.accept}
+                className="request-accept"
+                variant="success"
+              >
+                Accept
+              </Button>
+              <Button
+                onClick={this.props.delete.bind(this, request.id)}
+                className="request-decline"
+                variant="outline-danger"
+              >
+                Decline
+              </Button>
+            </span>
+          </div>
+        );
+      }
     });
-      
-    }
-
-
-  render(){
-let request = this.state.requestmade.map((request, i) => {
-  
-  if(this.props.connected){
     return (
-      <div className="request-box">
-      <img className="request-photo" src={request.profilepic} />
-      <h4 className="request-name">{request.name}</h4>
-      <span className="request-buttons">
-      <span>Connected!</span>
-      </span>
-    </div>
-    )
+      <div>
+        <Navigation user={this.props.user} />
 
-  } else if (this.props.del)  {
-return(
-  <div>
-    <h1>No Pending Messages</h1>
-  </div>
-)
-
-} 
-else{
- 
-  return (
-    <div className="request-box">
-   <img className="request-photo" src={request.profilepic} />
-   <h4 className="request-name">{request.name}</h4>
-   <span className="request-buttons">
-   <Button onClick={this.props.accept} className="request-accept" variant="success">Accept</Button>
-   <Button onClick={this.props.delete.bind(this, request.id)}className="request-decline" variant="outline-danger">Decline</Button>
-   </span>
- </div>)
+        <Container>
+          <Row>
+            <Col sm={8}>
+              <h1 className="pending-requests">
+                Pending <span className="cursive-requests">Requests</span>
+              </h1>
+              {request}
+            </Col>
+            <Col sm={4}>
+              <img className="background-requests" src={requestBackground} />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
   }
 }
-)
-  return (
-  <div>
-  <Navigation user={this.props.user}/>
-
-  <Container>
-    <Row>
-      <Col sm={8}>
-      <h1 className="pending-requests">Pending <span className="cursive-requests">Requests</span></h1>
-{request}
-      </Col>
-      <Col sm={4}>
-      <img className="background-requests" src={requestBackground} />
-      </Col>
-    </Row>
-  </Container>
-
-
-
-  </div>
-  );
-};
-}
-export default Requests;
