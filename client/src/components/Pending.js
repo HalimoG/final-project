3971e757159ff52ee2daee49ee165edaf4e5aa1f
Halimo <halimoghelle@gmail.com>
2019-05-08 08:27:35 -0400

    destructured state/props, initialized state variables as null

--- client/src/components/Pending.js
+++ client/src/components/Pending.js
@@ -1,68 +1,69 @@
-import React, {Component} from 'react';
-import '../pending.css';
-import requestBackground from '../collabrequestsphotog.jpg';
-import {Row, Col, Container, Button, Card} from 'react-bootstrap';
-import Navigation from './Navbar.js'
-const axios = require("axios");
+import React, { Component } from "react";
+import "../pending.css";
+import requestBackground from "../collabrequestsphotog.jpg";
+import { Row, Col, Container, Button } from "react-bootstrap";
+import Navigation from "./Navbar.js";
 
-class Pending extends Component {
+export default class Pending extends Component {
   constructor(props) {
     super(props);
-    this.state={
-      requestmade:[],
-      unsplashApi:[]
-    }
+    this.state = {
+      requestmade: null,
+      unsplashApi: null
+    };
   }
-  componentDidMount () {
-    var id = this.props.user.id
-    var url = '/'+id+'/influencerrequest'
+  componentDidMount() {
+    const { id } = this.props.user;
+    const url = "/" + id + "/influencerrequest";
     fetch(url)
-    .then(results => {
-      return results.json();
-    })
-    .then(data => { this.setState({requestmade:data})
-    })
-    .catch(function (error) {
-      console.log(error);
+      .then(results => {
+        return results.json();
+      })
+      .then(data => {
+        this.setState({ requestmade: data });
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
+
+  render() {
+    const { requestmade } = this.state;
+    const { user } = this.props;
+    let request = requestmade.map((request, i) => {
+      return (
+        <div className="request-box">
+          <img className="request-photo" src={request.profilepic} />
+          <h4 className="request-name">{request.name}</h4>
+          <h2 className="user-bio">{request.bio}</h2>
+          <span className="pending-buttons">
+            <Button className="request-pending" variant="success" disabled>
+              Pending
+            </Button>
+          </span>
+        </div>
+      );
     });
 
-    }
+    return (
+      <div>
+        <Navigation user={user} />
 
-  render(){
-let request = this.state.requestmade.map((request,i) => {
-   
-    return (<div className="request-box">
-    <img className="request-photo" src={request.profilepic} />
-    <h4 className="request-name">{request.name}</h4>
-    <h2 className = "photographer-bio" >{request.bio}</h2>
-      <span className="pending-buttons">
-    <Button className="request-pending" variant="success" disabled>Pending</Button>
-      </span>
-  </div>)
-})
+        <Container>
+          <Row>
+            <Col sm={8}>
+              <h1 className="pending-requests">
+                Pending <span className="cursive-requests">Connections</span>
+              </h1>
+              {request}
+            </Col>
 
-  return (
-  <div>
-  <Navigation user={this.props.user}/>
-
-  <Container>
-    <Row>
-      <Col sm={8}>
-      <h1 className="pending-requests">Pending <span className="cursive-requests">Connections</span></h1>
-       {request}
-      </Col>
-
-
-      <Col sm={4}>
-      <img className="background-requests" src={requestBackground} />
-      </Col>
-    </Row>
-  </Container>
-
-
-
-  </div>
-  );
-};
+            <Col sm={4}>
+              <img className="background-requests" src={requestBackground} />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
-export default Pending;
